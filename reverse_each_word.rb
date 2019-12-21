5b8ad99c21da49e4dde97f1033fe778cc9f1a145
Maylene Poulsen <mayleneski@hotmail.com>
2019-12-21 20:03:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,13 @@ def reverse_each_word(string)
   end  
   return_array.join(" ")
 end
+
+def reverse_each_word(string)
+  string_to_array = string.split
+  return_array = []
+  
+  string_to_array.collect do |word|
+    return_array.push(word.reverse)
+  end
+  return_array.join(" ")
+end  
